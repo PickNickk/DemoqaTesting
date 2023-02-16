e1a795909dfd641e707b23ce36c7989e2b2ab325
PickNickk <todojagami@gmail.com>
2023-02-16 17:27:18 +0500

    Cucumber test

---
+++ src/test/java/StepDefenition/TestSteps.java
@@ -0,0 +1,27 @@
+package StepDefenition;
+
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Тогда;
+import org.junit.jupiter.api.Assertions;
+
+public class TestSteps {
+    int x;
+    int y;
+    static int result;
+    @Когда("Клиент снял х = {int} тысяч рублей")
+    public void getX(int x){
+        this.x = x;
+    }
+    @Когда("Клиент снял y = {int} тысяч рублей")
+    public void getY(int y){
+        this.y = y;
+    }
+    @Тогда("^Получение наличных средств$")
+    public void getSum(){
+        result = x+y;
+    }
+    @Когда("^Общая сумма наличных средств: (.*)$")
+    public static void getResult(int res) {
+        Assertions.assertEquals(res,result);
+    }
+}
